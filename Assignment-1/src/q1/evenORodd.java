6914f9b2bce314f55d4fe4a0b13f3f280f5dc0f1
Sadman Rahman <sadmanrahman680@gmail.com>
2025-11-13 12:32:50 +0600

    Updated

--- Assignment-1/src/q1/evenORodd.java
+++ Assignment-1/src/q1/evenORodd.java
@@ -6,6 +6,7 @@ import java.io.*;
 public class evenORodd {
     public static void main(String[] args)throws IOException {
 
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Integer T = Integer.parseInt(br.readLine());
 
